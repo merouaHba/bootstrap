5e8cb3819a97f02b95dc29fc35c1cf503bc25817
maroua hba <marouahba31@gmail.com>
2022-10-24 18:45:33 +0100

    design

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// active navbar
+let nav = document.querySelector(".navbar")
+window.onscroll = () => {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("scroll-on")
+    }else{
+        nav.classList.remove("scroll-on")
+    }
+}
+
+
+// counter design
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    function counter(id,start,end,duration){
+        let obj = document.getElementById(id),
+        current = start,
+        range = end - start,
+        increment = end > start ? 1 : -1,
+        step = Math.abs(Math.floor(duration / range)),
+        timer = setInterval(() => {
+            current += increment
+            obj.textContent = current
+            if (current == end){
+                clearInterval(timer)
+            }
+        }, step)
+    }
+    counter("count1", 0, 1287, 3000)
+    counter("count2", 0, 1287, 3000)
+    counter("count3", 0, 1287, 3000)
+    counter("count4", 0, 1287, 3000)
+})
